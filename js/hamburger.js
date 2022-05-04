74639e92251d72c110d1e93a51a0e7003737e357
Arm0ny <63424081+Arm0ny@users.noreply.github.com>
2022-05-04 15:17:45 +0200

    solution for mobile nav buttons not working (to test) added refs to sections in lav links

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,22 +4,22 @@ var nav_ul = navbar.children[1];
 var arrow = navbar.children[2];
 
 if (window.matchMedia('(max-width: 1060px)')) {
-    hamburger.addEventListener('click', function () {
+    hamburger.onclick = function () {
         hamburger.style.display= 'none';
         navbar.classList.remove('navbar')
         navbar.classList.add('nav-responsive');
         navbar.children[0].classList.add('nav-logo-responsive');
         navbar.children[1].classList.add('nav-ul-responsive');
         arrow.style.display = 'block';
-    });
+    }
 
-    arrow.addEventListener('click', function(){
+    arrow.onclick = function(){
         navbar.classList.remove('nav-responsive');
         navbar.classList.add('navbar');
         navbar.children[0].classList.remove('nav-logo-responsive');
         navbar.children[1].classList.remove('nav-ul-responsive');
         arrow.style.display = 'none';
         hamburger.style.display= 'block';
-    });
+    }
 }
 
